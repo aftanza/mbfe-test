80478a902eea38d9a4c337322c5e1b91246af7f2
Aftanza <zazaprabowo@gmail.com>
2024-06-04 09:27:16 +0800

    Changed provinsi styles.scss to module form

--- app/(framed)/_components/block-4/_components/provinsi/index.tsx
+++ app/(framed)/_components/block-4/_components/provinsi/index.tsx
@@ -1,19 +1,17 @@
 import { ProvinsiProps } from "@/app/_globals/types";
-import "./styles.scss";
+import styles from "./provinsi.module.scss";
 import { Text } from "@mantine/core";
 
 const Provinsi = ({ provinsi }: ProvinsiProps) => {
     let yellow = false;
     return (
-        <div className="provinsi-list">
+        <div className={styles.provinsiList}>
             {provinsi.map((currentProvinsi, index) => {
                 yellow = !yellow;
                 return (
                     <Text
-                        className={`provinsi-list__provinsi ${
-                            yellow
-                                ? "provinsi-list__provinsi__yellow"
-                                : "provinsi-list__provinsi__blue"
+                        className={`${styles.provinsi} ${
+                            yellow ? styles.yellow : styles.blue
                         }`}
                         key={index}
                     >
